53c20388f517e1aaabd78098aa0d1cdcb50a5677
Nitya Jain <nityaprofessional6402@gmail.com>
2026-01-10 20:01:20 +0530

    slots add

--- frontend/src/lib/booking-utils.js
+++ frontend/src/lib/booking-utils.js
@@ -15,7 +15,7 @@ export const SESSION_TYPES = [
     id: "deep",
     title: "Deep Dive",
     duration: 30,
-    price: 1,  // TODO: Change back to 199 after testing
+    price: 0,  // TODO: Change back to 199 after testing
     description: "Detailed discussion about placements, faculty, and more",
     type: "1:1",
   },
@@ -111,7 +111,9 @@ export function generateMockSlots(mentorId, days = 7) {
   const timeSlots = [
     "09:00", "09:30", "10:00", "10:30", "11:00", "11:30",
     "12:00", "12:30", "14:00", "14:30", "15:00", "15:30",
-    "16:00", "16:30", "17:00", "17:30"
+    "16:00", "16:30", "17:00", "17:30", "18:00", "18:30",
+    "19:00", "19:30", "20:00", "20:30", "21:00", "21:30",
+    "22:00"
   ];
 
   dates.forEach((date) => {
