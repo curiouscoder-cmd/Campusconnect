9f31b016393e62119fbf98e46eb3b5e52a89d36a
Nitya Jain <nityaprofessional6402@gmail.com>
2026-01-11 15:47:05 +0530

    updated logo and favicons

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -44,7 +44,7 @@ export function Navbar() {
                     <img
                         src="/favicon_light.ico"
                         alt="Campus Connect"
-                        className="w-10 h-10 border-0 outline-none"
+                        className="w-13 h-12 border-0 outline-none"
                     />
                     <span className="font-semibold text-primary text-lg">Campus Connect</span>
                 </Link>
